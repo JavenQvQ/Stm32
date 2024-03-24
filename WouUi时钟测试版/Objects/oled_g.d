2ac0288f3571bd90b0592594fe8b38fbbd86fcdb
JavenQvQ <970280795@qq.com>
2024-03-24 16:26:47 +0800

    第二次更新

--- WouUi时钟测试版/Objects/oled_g.d
+++
@@ -1,8 +0,0 @@
-.\objects\oled_g.o: Hardware\oled_g.c
-.\objects\oled_g.o: Hardware\oled_g.h
-.\objects\oled_g.o: Hardware\oled_port.h
-.\objects\oled_g.o: D:\ruanjian\keil\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\oled_g.o: D:\ruanjian\keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\oled_g.o: D:\ruanjian\keil\ARM\ARMCC\Bin\..\include\string.h
-.\objects\oled_g.o: Hardware\oledfont.h
-.\objects\oled_g.o: D:\ruanjian\keil\ARM\ARMCC\Bin\..\include\math.h
--- WouUi时钟测试版/Objects/oled_ui.d
+++
@@ -1,8 +0,0 @@
-.\objects\oled_ui.o: Hardware\oled_ui.c
-.\objects\oled_ui.o: Hardware\oled_ui.h
-.\objects\oled_ui.o: Hardware\oled_g.h
-.\objects\oled_ui.o: Hardware\oled_port.h
-.\objects\oled_ui.o: D:\ruanjian\keil\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\oled_ui.o: D:\ruanjian\keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\oled_ui.o: Hardware\oled_conf.h
-.\objects\oled_ui.o: D:\ruanjian\keil\ARM\ARMCC\Bin\..\include\string.h
