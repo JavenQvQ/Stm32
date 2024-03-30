47a067bddd65632ad38bfd253eed37ef8ae47f36
JavenQvQ <970280795@qq.com>
2024-03-30 17:42:11 +0800

    main

--- FFT测量/System/IC.c
+++ FFT测量/System/IC.c
@@ -1,7 +1,30 @@
 #include "stm32f10x.h"
+uint16_t ADC_Value;//存放ADC值
+void TIM1_Init(void)
+{
+		TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+		TIM_OCInitTypeDef TIM_OCInitStructure;
+		
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); 		//时钟使能
+
+		TIM_TimeBaseStructure.TIM_Period = 30000-1; 		//设置在下一个更新事件装入活动的自动重装载寄存器周期的值
+		TIM_TimeBaseStructure.TIM_Prescaler =7200-1; 			//设置用来作为TIMx时钟频率除数的预分频值
+		TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;		//设置时钟分割:不分割
+		TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; 		//TIM向上计数模式
+		TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);			//根据指定的参数初始化TIMx的时间基数单位
+		
+		TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;		//选择定时器模式:TIM脉冲宽度调制模式1
+		TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;		//比较输出使能
+		TIM_OCInitStructure.TIM_Pulse = 9;
+		TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;		//输出极性:TIM输出比较极性低
+		TIM_OC2Init(TIM2, & TIM_OCInitStructure);		//初始化外设TIM2_CH2
+		
+		TIM_Cmd(TIM2, ENABLE); 			//使能TIMx
+}
 
 void ADC1_Init(uint32_t AddrB)
 {
+    TIM1_Init();
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
     RCC_ADCCLKConfig(RCC_PCLK2_Div6);//ADC时钟设置为PCLK2的6分频，即12MHz
@@ -16,15 +39,15 @@ void ADC1_Init(uint32_t AddrB)
     ADC_InitTypeDef ADC_1;
     ADC_1.ADC_Mode=ADC_Mode_Independent;//独立模式
     ADC_1.ADC_DataAlign=ADC_DataAlign_Right;//右对齐
-    ADC_1.ADC_ExternalTrigConv=ADC_ExternalTrigConv_None;//软件触发
+    ADC_1.ADC_ExternalTrigConv= ADC_ExternalTrigConv_T2_CC2;//外部触发源选择
     ADC_1.ADC_NbrOfChannel=1;//1个转换通道
     ADC_1.ADC_ScanConvMode=DISABLE;//非扫描模式
-    ADC_1.ADC_ContinuousConvMode=ENABLE;//连续转换模式
+    ADC_1.ADC_ContinuousConvMode=DISABLE;//连续转换模式
     ADC_Init(ADC1,&ADC_1);
 
     DMA_InitTypeDef DMA_InitStructure;
     DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;//要传输的数据的地址,外设
-    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)AddrB;//存放数据的地址,内存
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)&ADC_Value;//存放数据的地址,内存
     DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;//数据传输方向,外设到内存就是由A到B
     DMA_InitStructure.DMA_BufferSize = 1;//数据传输量,给传输寄存器的值
     DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;//要传输的地址不自增
@@ -45,10 +68,6 @@ void ADC1_Init(uint32_t AddrB)
     while(ADC_GetResetCalibrationStatus(ADC1)==SET);//等待复位校准结束
     ADC_StartCalibration(ADC1);//开始校准
     while(ADC_GetCalibrationStatus(ADC1)==SET);//等待校准结束
-    ADC_SoftwareStartConvCmd(ADC1,ENABLE);//开始转换
+    ADC_ExternalTrigConvCmd(ADC1, ENABLE);
 }
 
-void TIM1_Init(void)
-{
-	
-}
