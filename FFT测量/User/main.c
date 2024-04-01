b714f56c56b9f0569cc0eb0a059b5bb58b829e7a
JavenQvQ <970280795@qq.com>
2024-04-01 14:22:12 +0800

    main

--- FFT测量/User/main.c
+++ FFT测量/User/main.c
@@ -2,24 +2,57 @@
 #include "delay.h"
 #include "key.h"
 #include "oled.h"
-#include "Timer.h"
 #include "PWM.h"
 #include "IC.h"
-uint16_t frequency=1000;
-uint16_t frequency_Set=1;
-uint8_t KeyCode;
-uint16_t ADC_Value;
+#include "table_fft.h"
+#define NPT 1024
+uint32_t InBufArray[1024];//存放ADC值
+uint32_t OutBufArray[512];//存放FFT变换后的值
+uint32_t MagBufArray[512];//存放FFT变换后的值
+
+u8 flag;
+//FFT算法,计算幅值
+void GetPowerMag()
+{
+    signed short lX,lY;
+    float X,Y,Mag;
+    unsigned short i;
+	u16 temp;
+    for(i=0; i<512; i++)
+    {
+        lX  = (OutBufArray[i] << 16) >> 16;
+        lY  = (OutBufArray[i] >> 16);
+        
+        //除以32768再乘65536是为了符合浮点数计算规律
+        X = NPT * ((float)lX) / 32768;
+        Y = NPT * ((float)lY) / 32768;
+        Mag = sqrt(X * X + Y * Y) / NPT;
+        if(i == 0)
+            MagBufArray[i] = (unsigned long)(Mag * 32768);
+        else
+            MagBufArray[i] = (unsigned long)(Mag * 65536);   //MagBufArray[]为计算输出的幅值数组
+    }
+
+}
+
+
 int main()
 {
-	OLED_Init();
-	PWM_Init();
-	ADC1_Init((uint32_t)&ADC_Value);
-	Key_Init();
 	
+	OLED_Init();
+	ADC1_Init();
+	Key_Init();
 	while(1)
 	{
-		OLED_ShowNum(1,1,ADC_Value,4);
-		KeyCode=KEY_FIFO_Get();
+		if(flag==1)
+		{
+			flag=0;
+			cr4_fft_1024_stm32(InBufArray,OutBufArray,1024);//FFT算法
+			GetPowerMag();//计算幅值
+		}
+
+		
+
 
 	
  	} 
