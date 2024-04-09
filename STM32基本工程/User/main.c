2f81a52ab1736ceefcdf3d0908fd3cfeba9eb91d
J <970280795@qq.com>
2024-04-09 23:29:38 +0800

    STM32基本工程

---
+++ STM32基本工程/User/main.c
@@ -0,0 +1,17 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "TIMEEVEN.H"
+
+
+int main(void)
+{
+	/*模块初始化*/
+	OLED_Init();		//OLED初始化
+	
+	
+	while (1)
+	{
+		
+	}
+}
