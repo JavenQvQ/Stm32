fae3eefb92a9f4d8c780807053364f6fbfdc7cd7
J <970280795@qq.com>
2024-09-19 22:54:02 +0800

    深度检测

--- 加速度测试/User/main.c
+++ 加速度测试/User/main.c
@@ -1,3 +1,4 @@
+/*woshinidie*/
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include <math.h>
@@ -10,19 +11,20 @@
 
 #define Task1_ID 1//任务1的ID
 
-float G[3];
-float A[3];
-float Gz;
+#define GZ_PRESS_SIZE 40
 uint16_t countx = 0; // 用于按压次数计数
+uint16_t count = 0; // 用于按压次数计数
 int8_t GetDate_Flag = 0;
 float totalAcceleration;
 float AX1, AY1, AZ1;
+float V = 0;
+
 
 ADXL355_HandleTypeDef ADXL355_t;
 void ADXL355(void)
 {
-    ADXL355_t.ADXL355_Range = ADXL355_RANGE_2G;
-    ADXL355_t.ADXL355_LowPass = ADXL355_ODR_4000;
+    ADXL355_t.ADXL355_Range = ADXL355_RANGE_4G;
+    ADXL355_t.ADXL355_LowPass = ADXL355_ODR_2000;
     ADXL355_t.ADXL355_HighPass = 0x00;
     ADXL355_Init(&ADXL355_t);
 }
@@ -30,115 +32,83 @@ void ADXL355(void)
 
 void TASK1(void)
 {
-
+    static uint8_t state = 0;
     ADXL355_ReadData();
-    const float scale = 1.0f / ADXL355_RANGE_2G_SCALE;
+    const float scale = 1.0f / ADXL355_RANGE_4G_SCALE;
     AZ1 = (float)i32SensorZ * scale;
     AX1 = (float)i32SensorX * scale;
     AY1 = (float)i32SensorY * scale;
     arm_sqrt_f32(AX1 * AX1 + AY1 * AY1 + AZ1 * AZ1, &totalAcceleration);
-	static float Gz_Press[150];
-    static uint16_t i = 0;
-    static int8_t stage = 0; // 阶段标志
-	static uint8_t currentIndex = 0; // 当前检测的位置
-	static uint8_t Gz_Press_Start;//按压开始数组索引
-	static uint8_t Gz_Press_End;//按压结束数组索引
-	
-
-	//更新Gz_Press数组
-	Gz_Press[i] = totalAcceleration-0.98;
-    i = (i + 1) % 150;//循环数组
-
-   // 检测逻辑
-    int consecutiveCount = 0;//连续判断是否满足条件的计数
-    switch (stage)
+    totalAcceleration=totalAcceleration-0.98;
+    switch(state)
     {
-    case 0:
-        // 检测至少连续20个数字小于-0.01
-        for (int j = currentIndex; j < currentIndex + 150; j++)
-        {
-            int index = j % 150; // 确保索引在数组范围内
-            if (Gz_Press[index] < -0.01)
+        case 0://加速度小于0的情况
+            if(countx >= 30) 
             {
-				if (consecutiveCount == 0) {
-                    Gz_Press_Start = index; // 记录开始检测的位置
-                }
-                consecutiveCount++;
-			if (consecutiveCount >= 20)
-            {
-                // 如果Gz的值大于0.1，进入阶段1
-                if (Gz_Press[index+1] > 0.1)
+                if(totalAcceleration >= 0) 
                 {
-                    stage = 1;
-                    currentIndex = index; // 更新当前检测的位置
+                    countx = 0;
+                    state = 1;
                 }
-                break;
             }
-            }
-            else
+            if(totalAcceleration <= 0) 
             {
-                consecutiveCount = 0;
+                countx++;
+                V=V+totalAcceleration;
             }
-
-        }
-        break;
-    case 1:
-        // 检测至少连续40个数字大于0.01
-        for (int j = currentIndex; j < currentIndex + 150; j++)
-        {
-            int index = j % 150; // 确保索引在数组范围内
-            if (Gz_Press[index] > 0.01)
+            else 
             {
-                consecutiveCount++;
-                if (consecutiveCount >= 20)
+                countx = 0;
+                V=0;
+            }
+            break;
+        case 1://加速度大于0的情况
+            if(countx >= 30) 
+            {
+                if(totalAcceleration <= 0) 
                 {
-                    // 如果Gz的值小于-0.01，进入阶段2
-                    if (Gz_Press[index+1] < -0.01)
-                    {
-                        stage = 2;
-                        currentIndex = index; // 更新当前检测的位置
-                    }
-                    break;
+                    countx = 0;
+                    state = 2;
                 }
             }
-            else
+            if(totalAcceleration >= 0) 
             {
-                consecutiveCount = 0;
+                countx++;
+                V=V+totalAcceleration;
             }
-        }
-        break;
-    case 2:
-        // 检测至少连续40个数字小于-0.01
-        for (int j = currentIndex; j < currentIndex + 150; j++)
-        {
-            int index = j % 150; // 确保索引在数组范围内
-            if (Gz_Press[index] < -0.01)
+            else 
             {
-                consecutiveCount++;
-                if (consecutiveCount >= 20)
+                countx = 0;
+                V=0;
+                state = 0;
+            }
+            break;
+        case 2://加速度小于0的情况
+            if(countx >= 30) 
+            {
+                if(totalAcceleration >= 0) 
                 {
-					if(Gz_Press[index+1] > 0)
-					{
-						countx++;
-                    	stage = 0;
-                   		// 清空Gz_Press数组
-						memset(Gz_Press, 0, sizeof(Gz_Press));//清空数组
-                    	i = 0;
-                    	currentIndex = 0; // 重置当前检测的位置
-                    	break;
-					}
-
+                    countx = 0;
+                    count++;
+                    state = 0;
                 }
             }
-            else
+            if(totalAcceleration <= 0) 
             {
-                consecutiveCount = 0;
+                countx++;
+                V=V+totalAcceleration;
             }
-        }
-        break;
+            else 
+            {
+                countx = 0;
+                V=0;
+                state = 0;
+            }
+            break;
     }
-     printf("%.3f\n",totalAcceleration);
-}
+    printf("%f\n",totalAcceleration);            
+                
+ }
 /** 
  * @brief  校准函数
  * @param  无
@@ -160,7 +130,7 @@ int main(void)
 			TASK1();
 			GetDate_Flag = 0;
 		}
-		OLED_ShowNum(4, 1, countx, 3);	//显示按压次
+		OLED_ShowNum(4, 1, count, 3);	//显示按压次
 	}
 }
 
@@ -177,3 +147,53 @@ void TIM2_IRQHandler(void)
     
 
 }
+
+	// switch(stage)
+    // {
+    //     case 0:
+    //             if(totalAcceleration <= 0) 
+    //             {
+    //                 count++;
+    //                 V=V+totalAcceleration;
+    //             } 
+    //             else 
+    //             {
+    //                 count = 0;
+    //                 V=0;
+    //             }
+    //             if(count >= 40) 
+    //             {
+    //                 V = V * 0.01; // 计算速度
+    //                 stage = 1;//进入1阶段
+    //                 count = 0;//清零
+    //             }
+    //             break;
+    //     case 1:
+    //             V += totalAcceleration*0.01;
+    //             if(V >= 0)//速度为0,到达原来点且加速度大于0.25开始准备返回
+    //             {
+    //                 if(totalAcceleration > 0)
+    //                 {
+    //                     V = 0;
+    //                     stage = 2;
+    //                 }
+    //                 else
+    //                 {
+    //                     V = 0;
+    //                     stage = 0;
+    //                 }
+
+    //             }
+    //             break;
+    //     case 2:
+    //             V += totalAcceleration*0.01;
+    //             if(totalAcceleration <= 0)//在回去的过程中开始减速
+    //             {
+    //                 stage = 3;
+    //             }
+    //             break;
+    //     case 3:
+    //             V += totalAcceleration*0.01;
+                
+    // }
+    // //printf("%f\n",totalAcceleration);
